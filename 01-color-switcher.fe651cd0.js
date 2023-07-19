cb2e6b4332b432ada78cda991eed67811f441a44
Artem303 <125798638+Artem303@users.noreply.github.com>
2023-07-19 17:52:57 +0000

    Deploying to gh-pages from @ Artem303/goit-js-hw-09@2849a470f9be13da00869346b8092a44d5581ce8 🚀

---
+++ 01-color-switcher.fe651cd0.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("body"),e=document.querySelector("[data-start]"),r=document.querySelector("[data-stop]");r.setAttribute("disabled","true"),e.addEventListener("click",(function(){timerId=setInterval((()=>{t.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),e.setAttribute("disabled","true"),r.removeAttribute("disabled")})),r.addEventListener("click",(function(){clearInterval(timerId),e.removeAttribute("disabled"),r.setAttribute("disabled","true")}));
+//# sourceMappingURL=01-color-switcher.fe651cd0.js.map
