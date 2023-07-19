2849a470f9be13da00869346b8092a44d5581ce8
Artem303 <125798638+Artem303@users.noreply.github.com>
2023-07-19 20:52:07 +0300

    first commit

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,28 @@
+const bodyElement = document.querySelector('body');
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+btnStop.setAttribute('disabled', 'true');
+
+btnStart.addEventListener('click', onStartChangeColor);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function onStartChangeColor() {
+  timerId = setInterval(() => {
+    bodyElement.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  btnStart.setAttribute('disabled', 'true');
+  btnStop.removeAttribute('disabled');
+}
+
+btnStop.addEventListener('click', onStopChange);
+
+function onStopChange() {
+  clearInterval(timerId);
+  btnStart.removeAttribute('disabled');
+  btnStop.setAttribute('disabled', 'true');
+}
